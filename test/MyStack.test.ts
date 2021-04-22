bd29decdb09a7da170841f839c8e2c29cb06f03e
Cole Groff <groffcole@gmail.com>
2021-04-22 07:08:12 -0500

    initial commit

---
+++ test/MyStack.test.ts
@@ -0,0 +1,20 @@
+import { expect, haveResource } from "@aws-cdk/assert";
+import * as sst from "@serverless-stack/resources";
+import MyStack from "../lib/MyStack";
+
+test("Test Stack", () => {
+  const app = new sst.App();
+  // WHEN
+  const stack = new MyStack(app, "test-stack");
+  // THEN
+  expect(stack).to(haveResource("AWS::Lambda::Function"));
+});
+
+
+test("Test Stack 2", () => {
+  const app = new sst.App();
+  // WHEN
+  const stack = new MyStack(app, "test-stack");
+  // THEN
+  expect(stack).to(haveResource("AWS::Lambda::Function"));
+});
